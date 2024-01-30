9aa6a39246c9ea011cbe5f987fb649b852bc7991
Margarita <rita.amazon01@gmail.com>
2024-01-29 19:11:14 -0600

    first code

---
+++ node.js
@@ -0,0 +1,5 @@
+let firstNum = 2 * 5;
+let secondNum = 60 - 20;
+let thirdNum = 20 + 19;
+const string = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${firstNum} - ${secondNum} - ${thirdNum}`;
+alert(string);
